d52ba30e912a8da00290905f8e8b15f26e02eedc
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2024-11-25 00:00:09 +0300

    test

--- src/components/Categories/Category.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Categories.css"
-const Category = ({img,categoryTitle}) => {
-  return (
-    <div className='category-item'><img src={img} alt="" /><h5>{categoryTitle}</h5></div>
-  )
-}
-
-export default Category
